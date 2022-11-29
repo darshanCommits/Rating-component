4dc82a9f7031eb78ff24ccc467642c9acd7b351f
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2022-11-29 18:56:27 +0530

    basic ui done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      navy : "hsl(216, 12%, 8%)",
+      white : "#fff",
+      grey : "hsl(217, 12%, 63%)",
+      darkGrey : "hsl(216, 12%, 54%)",
+      blue : "#161E27",
+      orange : "hsl(25, 97%, 53%)",
+      circle : "#262F38"
+    },
+
+    fontFamily: {
+      serif : ['Overpass', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
